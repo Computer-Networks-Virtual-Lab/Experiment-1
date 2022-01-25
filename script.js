40dc8b4c6f077c3d86a0e4955b12a8c3fa67b129
tanishabisht <agamyatani@gmail.com>
2022-01-25 10:19:48 +0530

    fabricate cable: add reset

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var bezierWeight = 0.675;
 var handles = document.querySelectorAll(".handle");
 var path = document.querySelectorAll(".path");
 var path_w = document.querySelectorAll(".path_w");
+var resetBtn = document.getElementById("resetBtn");
 
 TweenLite.set(handles[0], { x: x2, y: y1 });
 TweenLite.set(handles[1], { x: x1, y: y1 });
@@ -170,3 +171,116 @@ function updatePath() {
   path[6].setAttribute("d", data7);
   path[7].setAttribute("d", data8);
 }
+
+function onReset() {
+  console.log("ON RESET");
+
+  var x11 = x2;
+  var y11 = y1;
+  var x12 = x1;
+  var y12 = y1;
+
+  var x21 = x2;
+  var y21 = y2;
+  var x22 = x1;
+  var y22 = y2;
+
+  var x31 = x2;
+  var y31 = y3;
+  var x32 = x1;
+  var y32 = y3;
+
+  var x41 = x2;
+  var y41 = y4;
+  var x42 = x1;
+  var y42 = y4;
+
+  var x51 = x2;
+  var y51 = y5;
+  var x52 = x1;
+  var y52 = y5;
+
+  var x61 = x2;
+  var y61 = y6;
+  var x62 = x1;
+  var y62 = y6;
+
+  var x71 = x2;
+  var y71 = y7;
+  var x72 = x1;
+  var y72 = y7;
+
+  var x81 = x2;
+  var y81 = y8;
+  var x82 = x1;
+  var y82 = y8;
+
+  var dx1 = Math.abs(x12 - x11) * bezierWeight;
+  var dx2 = Math.abs(x22 - x21) * bezierWeight;
+  var dx3 = Math.abs(x32 - x31) * bezierWeight;
+  var dx4 = Math.abs(x42 - x41) * bezierWeight;
+  var dx5 = Math.abs(x52 - x51) * bezierWeight;
+  var dx6 = Math.abs(x62 - x61) * bezierWeight;
+  var dx7 = Math.abs(x72 - x71) * bezierWeight;
+  var dx8 = Math.abs(x82 - x81) * bezierWeight;
+
+  var _x11 = x11 - dx1;
+  var _x12 = x12 + dx1;
+  var _x21 = x21 - dx2;
+  var _x22 = x22 + dx2;
+  var _x31 = x31 - dx3;
+  var _x32 = x32 + dx3;
+  var _x41 = x41 - dx4;
+  var _x42 = x42 + dx4;
+  var _x51 = x51 - dx5;
+  var _x52 = x52 + dx5;
+  var _x61 = x61 - dx6;
+  var _x62 = x62 + dx6;
+  var _x71 = x71 - dx7;
+  var _x72 = x72 + dx7;
+  var _x81 = x81 - dx8;
+  var _x82 = x82 + dx8;
+
+  var data1 = `M${x11} ${y11} C ${_x11} ${y11} ${_x12} ${y12} ${x12} ${y12}`;
+  var data2 = `M${x21} ${y21} C ${_x21} ${y21} ${_x22} ${y22} ${x22} ${y22}`;
+  var data3 = `M${x31} ${y31} C ${_x31} ${y31} ${_x32} ${y32} ${x32} ${y32}`;
+  var data4 = `M${x41} ${y41} C ${_x41} ${y41} ${_x42} ${y42} ${x42} ${y42}`;
+
+  var data5 = `M${x51} ${y51} C ${_x51} ${y51} ${_x52} ${y52} ${x52} ${y52}`;
+  var data6 = `M${x61} ${y61} C ${_x61} ${y61} ${_x62} ${y62} ${x62} ${y62}`;
+  var data7 = `M${x71} ${y71} C ${_x71} ${y71} ${_x72} ${y72} ${x72} ${y72}`;
+  var data8 = `M${x81} ${y81} C ${_x81} ${y81} ${_x82} ${y82} ${x82} ${y82}`;
+
+  path_w[0].setAttribute("d", data1);
+  path_w[1].setAttribute("d", data2);
+  path_w[2].setAttribute("d", data3);
+  path_w[3].setAttribute("d", data4);
+
+  path[0].setAttribute("d", data1);
+  path[1].setAttribute("d", data2);
+  path[2].setAttribute("d", data3);
+  path[3].setAttribute("d", data4);
+  path[4].setAttribute("d", data5);
+  path[5].setAttribute("d", data6);
+  path[6].setAttribute("d", data7);
+  path[7].setAttribute("d", data8);
+
+  TweenLite.set(handles[0], { x: x2, y: y1 });
+  TweenLite.set(handles[1], { x: x1, y: y1 });
+  TweenLite.set(handles[2], { x: x2, y: y2 });
+  TweenLite.set(handles[3], { x: x1, y: y2 });
+  TweenLite.set(handles[4], { x: x2, y: y3 });
+  TweenLite.set(handles[5], { x: x1, y: y3 });
+  TweenLite.set(handles[6], { x: x2, y: y4 });
+  TweenLite.set(handles[7], { x: x1, y: y4 });
+  TweenLite.set(handles[8], { x: x2, y: y5 });
+  TweenLite.set(handles[9], { x: x1, y: y5 });
+  TweenLite.set(handles[10], { x: x2, y: y6 });
+  TweenLite.set(handles[11], { x: x1, y: y6 });
+  TweenLite.set(handles[12], { x: x2, y: y7 });
+  TweenLite.set(handles[13], { x: x1, y: y7 });
+  TweenLite.set(handles[14], { x: x2, y: y8 });
+  TweenLite.set(handles[15], { x: x1, y: y8 });
+}
+
+resetBtn.addEventListener("click", onReset);
